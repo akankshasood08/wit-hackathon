3ed9b41e6cd8f118a6dbacc01b7f78d2a78ccb47
Jeffrey Zhu <jeffreyzhu@Jeffreys-MacBook-Air.local>
2023-09-08 11:03:27 +1000

    finished

--- src/pages/therapists/script.js
+++ src/pages/therapists/script.js
@@ -2,7 +2,7 @@ let therapists = {
     data:[{
             Name: "Dylan Nicholls",
             Phone: "0401 123 424",
-            Email: "DylanNicholls@gmail.com",
+            Email: "DNicholls@gmail.com",
             Skills: "Addiction",
             Access: "Online, In Person"
         },
@@ -27,6 +27,104 @@ let therapists = {
             Skills: "Addiction",
             Access: "Online"
         },
+        {
+            Name: "Patty Mills",
+            Phone: "0428 575 499",
+            Email: "PMills@gmail.com",
+            Skills: "PTSD",
+            Access: "In Person"
+        },
+        {
+            Name: "Stan Grant",
+            Phone: "0488 172 489",
+            Email: "SGrant@gmail.com",
+            Skills: "Youth",
+            Access: "Online, In Person",
+        },
+        {
+            Name: "Brooke Boney",
+            Phone: "0412 475 992",
+            Email: "BBoney@gmail.com",
+            Skills: "Depression",
+            Access: "Online"
+        },
+        {
+            Name: "Bronwyn Bancroft",
+            Phone: "0488 287 577",
+            Email: "BBancroft@gmail.com",
+            Skills: "Domestic-Violence",
+            Access: "In Person"
+        },
+        {
+            Name: "David Gulpilil",
+            Phone: "0482 587 309",
+            Email: "DGulpilil@gmail.com",
+            Skills: "Depression",
+            Access: "Online"
+        },
+        {
+            Name: "Casey Donovan",
+            Phone: "0482 747 318",
+            Email: "CDonovan@gmail.com",
+            Skills: "PTSD",
+            Access: "In Person"
+        },
+        {
+            Name: "John Citizen",
+            Phone: "0400 000 000",
+            Email: "JCitizen@gmail.com",
+            Skills: "PTSD",
+            Access: "Online"
+        },
+        {
+            Name: "Neville Bonner",
+            Phone: "0423 858 402",
+            Email: "NBonner@gmail.com",
+            Skills: "Youth",
+            Access: "Online"
+        },
+        {
+            Name: "Adam Goodes",
+            Phone: "0438 049 588",
+            Email: "AGoodes@gmail.com",
+            Skills: "Domestic-Violence",
+            Access: "In Person"
+        },
+        {
+            Name: "Brandon Walters",
+            Phone: "0498 903 866",
+            Email: "BWalters@gmail.com",
+            Skills: "Anxiety",
+            Access: "Online, In person"
+        },
+        {
+            Name: "David Unaipon",
+            Phone: "0492 774 126",
+            Email: "DUnaipon@gmail.com",
+            Skills: "Addiction",
+            Access: "Online"
+        },
+        {
+            Name: "Archie Roach",
+            Phone: "0455 291 547",
+            Email: "ARoach@gmail.com",
+            Skills: "Depression",
+            Access: "Online"
+        },
+        {
+            Name: "Albert Namatjira",
+            Phone: "0432 866 271",
+            Email: "ANamatjira@gmail.com",
+            Skills: "PTSD",
+            Access: "Online"
+        },
+        {
+            Name: "Ben Harradine",
+            Phone: "0488 872 301",
+            Email: "BHarradine@gmail.com",
+            Skills: "Youth",
+            Access: "In Person"
+        }
     ],
 };
 
@@ -44,12 +142,12 @@ for (let i of therapists.data) {
 
     let phone = document.createElement("div");
     phone.classList.add("Phone");
-    phone.innerText = i.Phone;
+    phone.innerText = "Phone: " + i.Phone;
     container.appendChild(phone)
 
     let email = document.createElement("Email");
     email.classList.add("Email");
-    email.innerText = i.Email + '\n';
+    email.innerText = "Email: "+ i.Email + '\n';
     container.appendChild(email);
 
     // let skills = document.createElement("Skills");
@@ -59,7 +157,7 @@ for (let i of therapists.data) {
 
     let access = document.createElement("Access");
     access.classList.add("Access");
-    access.innerText = i.Access;
+    access.innerText = "Location: " + i.Access;
     container.appendChild(access);
 
     card.appendChild(container);
